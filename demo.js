887dd43b839b017c05d775270b7d46a612aaca2b
Tony Alejandro <tonysviews@gmail.com>
2019-11-28 19:37:52 +0800

    use mv-linear-icon-demo and mv-toast-demo implement sidebar to load demos

--- demo.js
+++ demo.js
@@ -1,8 +1,7 @@
 import { LitElement, html, css } from "lit-element";
 import "mv-menu-panel";
-import "mv-toast";
 import "mv-toast-demo";
-import "mv-linear-icon";
+import "mv-linear-icon-demo";
 
 export class MvFrontendDemo extends LitElement {
   static get properties() {
@@ -18,38 +17,83 @@ export class MvFrontendDemo extends LitElement {
         font-size: var(--font-size-m, 10pt);
       }
 
-      mv-toast {
-        margin: 5px;
-      }
-
       .main-content {
         margin-left: 350px;
       }
 
-      .toasts-container {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;        
+      small {
+        font-size: 12px;
+      }
+
+      mv-menu-panel[label] {
+        display: block;
+        width: 100%;
+        cursor: pointer;
       }
     `;
   }
 
+  constructor() {
+    super();
+    this.demos = {
+      "mv-toast": html`<mv-toast-demo></mv-toast-demo>`,
+      "mv-linear-icons": html`<mv-linear-icon-demo></mv-linear-icon-demo>`
+    };
+  }
+
   render() {
     return html`
-    <mv-menu-panel menu showLabel>      
-      <mv-menu-panel label>Meveo</mv-menu-panel>
+    <mv-menu-panel menu showLabel @select-header="${() => {
+      this.selectedDemo = null;
+    }}">      
+      <mv-menu-panel label>Menu <small>(using mv-menu-panel)</small></mv-menu-panel>
+      ${Object.keys(this.demos).map(
+        key => html`
+        <mv-menu-panel
+          item
+          .value="${key}"
+          .selected="${this.selectedDemo === key}"
+          @select-item="${this.handleMenuSelect}"
+        >${key}</mv-menu-panel>
+      `
+      )}
+      <mv-menu-panel group>
+        <mv-menu-panel label>Multi-level menu</mv-menu-panel>
+        <mv-menu-panel item disabled>1.1 <small>(disabled)</small></mv-menu-panel>
+        <mv-menu-panel item selected>1.2 <small>(selected)</small></mv-menu-panel>
+        <mv-menu-panel group>
+          <mv-menu-panel label>1.3</mv-menu-panel>
+          <mv-menu-panel item>1.3.1</mv-menu-panel>
+          <mv-menu-panel item>1.3.2</mv-menu-panel>
+          <mv-menu-panel item>1.3.3</mv-menu-panel>
+          <mv-menu-panel item>1.3.4</mv-menu-panel>
+          <mv-menu-panel group>
+            <mv-menu-panel label>1.3.5</mv-menu-panel>
+            <mv-menu-panel item>1.3.5.1</mv-menu-panel>
+            <mv-menu-panel item>1.3.5.2</mv-menu-panel>
+          </mv-menu-panel>
+        </mv-menu-panel>
+      </mv-menu-panel>
     </mv-menu-panel>
     <div class="main-content">
       <h1>Meveo Frontend Kitchen Sink Demo</h1>
-
-      <div class="toasts-container">
-        <mv-toast>Default parameters</mv-toast>
-        <mv-toast type="information">type: "information"</mv-toast>
-      </div>
-      <div class="icons-container">
-        <mv-linear-icon icon="heart"></mv-linear-icon> Heart icon
-      </div>
+      ${Object.keys(this.demos).map(
+        key => (key === this.selectedDemo ? this.demos[key] : html``)
+      )}
+      ${!this.selectedDemo
+        ? html`
+            <div>
+              This is the demo page for Meveo Framework components. <br />
+              Select a component from the menu to see it in action.
+            </div>`
+        : html``}
     </div>    
     `;
+  }
+
+  handleMenuSelect(event) {
+    const { detail: { value } } = event;
+    this.selectedDemo = value;
   }
 }
 
