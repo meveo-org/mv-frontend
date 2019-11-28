d29d58adb36b9a77b368ac4a223768b67111c223
Tony Alejandro <tonysviews@gmail.com>
2019-11-28 18:16:17 +0800

    add mv-linear-icon to submodules update mv-toast submodule

--- demo.js
+++ demo.js
@@ -1,10 +1,13 @@
 import { LitElement, html, css } from "lit-element";
+import "mv-menu-panel";
 import "mv-toast";
+import "mv-toast-demo";
+import "mv-linear-icon";
 
 export class MvFrontendDemo extends LitElement {
   static get properties() {
     return {
-      value: { type: String, attribute: true }
+      selectedDemo: { type: String, reflect: true, attribute: false }
     };
   }
 
@@ -14,24 +17,38 @@ export class MvFrontendDemo extends LitElement {
         font-family: var(--font-family, Arial);
         font-size: var(--font-size-m, 10pt);
       }
-      .toasts-container {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;        
-      }
 
       mv-toast {
         margin: 5px;
+      }
+
+      .main-content {
+        margin-left: 350px;
+      }
+
+      .toasts-container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;        
       }
     `;
   }
 
   render() {
     return html`
-    <h1>Meveo Frontend Kitchen Sink Demo</h1>
-    <div class="toasts-container">
-    <mv-toast>Default parameters</mv-toast>
-    <mv-toast type="information">type: "information"</mv-toast>
-    </div>
+    <mv-menu-panel menu showLabel>      
+      <mv-menu-panel label>Meveo</mv-menu-panel>
+    </mv-menu-panel>
+    <div class="main-content">
+      <h1>Meveo Frontend Kitchen Sink Demo</h1>
+
+      <div class="toasts-container">
+        <mv-toast>Default parameters</mv-toast>
+        <mv-toast type="information">type: "information"</mv-toast>
+      </div>
+      <div class="icons-container">
+        <mv-linear-icon icon="heart"></mv-linear-icon> Heart icon
+      </div>
+    </div>    
     `;
   }
 }
