6dc7bcd74c2eb1c86ef04ee68efd98d5eafc6a77
Guillaume Guarin <guillaume.guarin@webdrone.fr>
2023-01-09 17:54:52 +0100

    webpack config and package.json updated - elements are not displayed

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,23 +1,44 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.txt$/i,
+        use: 'raw-loader',
+      }
+    ],
+  },
     entry: './demo.js',
     mode: "development",
     output: {
-      path: path.resolve(__dirname, 'docs'),
-      publicPath: 'docs',
+      path: path.resolve(__dirname, './docs'),
       filename: 'index.js',
     },
     stats:{
       errorDetails: true
     },
     devServer: {
-      open: ['/index.html'],
+      open: ['./index.html'],
       static: {
         directory: path.join(__dirname, ''),
       },
       compress: true,
       port: 9000,
+      hot: false,
+      liveReload: true,
+      client: {
+        overlay: false
+      }
      
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: "./docs/index.html"
+    })]
   };
+
+
+
+
